e4ebfb106c9b1622b99015b8dcee25c771ebb269
Wa Kayakez <wa.kayakez.kayakez@student.ehb.be>
2022-10-05 14:09:30 +0200

    Code for straight red line

--- exercises/1-functions/1-1-WaKayakez.js
+++ exercises/1-functions/1-1-WaKayakez.js
@@ -1,1 +1,18 @@
 "use strict";
+
+drawLine();
+
+funtion drawline(){
+
+    let canvas = document.querySelector("canvas")
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d")
+
+    context.lineWidth = 5;
+    context.strokeStyle = 'red';
+    context.beginPath();
+    context.moveTo(175, 50);
+    context.lineTo(300,50);
+    context.stroke();
+}
